0cf7ca920a30beba3abe61fb72d32e3dd0eba8d7
Francisco Saavedra <saavedra.francisco@outlook.com>
2021-07-25 18:38:05 -0500

    initial files

---
+++ War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Francisco Saavedra on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
